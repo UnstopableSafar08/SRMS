b71d8556119728c7db9a79df7092a5a64ec86b8e
Sagar Malla <42813273+UnstopableSafar08@users.noreply.github.com>
2022-05-24 13:48:09 +0545

    Update init.php
    username added

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "13.235.243.98";
-	$username = "root";
-	$password = "@dmin#brain";
+	$username = "sagar";
+	$password = "sagar@123";
 	$conn = mysqli_connect($servername, $username, $password);
 	$db = mysqli_select_db($conn,'srms');
 
